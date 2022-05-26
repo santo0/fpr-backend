eb8add4d381be1dbce960aaa57884476b3a86540
santo0 <lemarti3472@gmail.com>
2022-05-26 18:52:54 +0200

    fixes and temporal development documentation

--- microservices/idea-service/flask_app/api/route/idea_route.py
+++ microservices/idea-service/flask_app/api/route/idea_route.py
@@ -5,19 +5,50 @@ idea_api = Blueprint('idea_api', __name__)
 
 @idea_api.route('/idea/<idea_id>', methods=['GET'])
 def get_idea(idea_id):
+    # read database
+    # notify user logging service via event/message broker
     raise NotImplementedError()
 
 
 @idea_api.route('/idea', methods=['POST'])
 def create_idea():
+    # write database
+    # notify user service
+    # notify search service
     raise NotImplementedError()
 
 
 @idea_api.route('/idea/<idea_id>', methods=['PUT'])
 def edit_idea(idea_id):
+    # write database
+    # notify user service
+    # notify search service
     raise NotImplementedError()
 
 
 @idea_api.route('/idea/<idea_id>', methods=['DELETE'])
 def delete_idea(idea_id):
+    # write database
+    # notify user service
+    # notify search service
+    # notify feed service
+    raise NotImplementedError()
+
+
+@idea_api.route('/idea/<idea_id>/follow', methods=['PUT'])
+def follow_idea(idea_id):
+    # write database
+    # notify user service
+    # notify user logging service
+    # notify feed service
+    # notify notification service
+    raise NotImplementedError()
+
+
+@idea_api.route('/idea/<idea_id>/unfollow', methods=['DELETE'])
+def unfollow_idea(idea_id):
+    # write database
+    # notify user service
+    # notify feed service
+    # notify user logging service
     raise NotImplementedError()
--- microservices/idea-service/flask_app/api/route/post_route.py
+++ microservices/idea-service/flask_app/api/route/post_route.py
@@ -5,27 +5,45 @@ post_api = Blueprint('post_api', __name__)
 
 @post_api.route('/posts/<idea_id>', methods=['GET'])
 def get_posts(idea_id):
+    # read database
+    # notify user logging service
     raise NotImplementedError()
 
 
 @post_api.route('/post/<post_id>', methods=['GET'])
 def get_post(post_id):
+    # read database
+    # notify user logging service
     raise NotImplementedError()
 
 
 @post_api.route('/post/<idea_id>', methods=['POST'])
 def create_post(idea_id):
+    # write database
+    # notify user service
+    # notify feed service
     raise NotImplementedError()
 
 
 @post_api.route('/post/<post_id>', methods=['DELETE'])
 def delete_post(post_id):
+    # write database
+    # notify user service
     raise NotImplementedError()
+
 
 @post_api.route('/post/<post_id>/like', methods=['PUT'])
 def like_post():
+    # write database
+    # notify user service
+    # notify notification service
+    # notify feed service
     raise NotImplementedError()
+
 
 @post_api.route('/post/<post_id>/like', methods=['DELETE'])
 def unlike_post(post_id):
+    # write database
+    # notify user service
+    # notify feed service
     raise NotImplementedError()
